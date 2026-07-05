124860f243f08837a9d9ca72fcd75566c7d7f198
hdelgado84 <63519583+hdelgado84@users.noreply.github.com>
2026-07-04 23:35:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use((req,res,next) => {
 app.use("/auth", loginRoute)
 //app.use(authentication);
 
-app.use('/', productsRouter);  // /apy en vez /
+app.use('/api', productsRouter);  // /apy en vez /
 
 
 /*http://localhost:3000/ping */
